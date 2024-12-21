43692355a1e2c0c064f180371ded7ab4b0ddc50a
medallo22 <alejoromero2508@gmail.com>
2024-12-21 12:41:22 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// config.php
+ 
+// Configuración de las credenciales de Telegram
+define('TELEGRAM_BOT_TOKEN', '7196050445:AAG2GK1qq7_m5JJEronwT19-8vEkUWA3Ris');
+define('TELEGRAM_CHAT_ID', '7299286232');
+?>
